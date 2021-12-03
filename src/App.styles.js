d1083ddadc0a85c14d2f3a12d4160101e788d9aa
JillBirchall <jill.m.birchall@gmail.com>
2021-12-03 13:13:14 +0000

    Fixing layout issue

--- src/App.styles.js
+++ src/App.styles.js
@@ -8,7 +8,6 @@ export const Wrapper = styled.div`
   min-height: 100vh;
   width: 100%;
   background: ${(props) => props.theme.colours.backgroundColour};
- 
 
   .quiz-box {
     display: flex;
@@ -27,43 +26,32 @@ export const Wrapper = styled.div`
   }
 
   .title {
-    font-family:  ${(props) => props.theme.fonts.primaryFont};
+    font-family: ${(props) => props.theme.fonts.primaryFont};
     align-self: center;
-    color:  ${(props) => props.theme.colours.headingColour};
+    color: ${(props) => props.theme.colours.headingColour};
     font-size: 1.5rem;
   }
 
-  @media (min-width: 768px) {
+  @media (min-width: 992px) {
     .quiz-box {
       width: 70%;
     }
   }
 
-  @media (orientation: landscape) and (min-width: 550px) {
+  @media (orientation: landscape) and (min-width: 768px) {
+    .title {
+      font-size: 2rem;
+    }
+  }
+
+  @media (min-height: 500px) {
     .quiz-box {
-      width: 60%;
+      min-height: 300px;
     }
 
-    @media (orientation: landscape) and (min-width: 768px) {
-
-      .title {
-        font-size: 2rem;
-      }
-
-      .quiz-box {
-        min-height: 350px;
-      }
+    .title {
+      align-self: center;
+      font-size: 2.5rem;
     }
-    
-@media (min-height: 500px) {
-
-  .quiz-box {
-    min-height: 300px;
   }
-
-  .title {
-    align-self: center;
-    font-size: 2.5rem;
-  }
-}
 `;
